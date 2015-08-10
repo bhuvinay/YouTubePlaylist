ee7d930f58fea4388857db621817d6267121a7e1
bhuvinay <bhu.vinay@gmail.com>
2015-08-10 23:30:44 +0530

    YouTubePlayList
    youtube playlist like Demo app using 3rd party library like Picaso,
    okhttp

---
+++ gen/com/example/youtubeplaylist/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.example.youtubeplaylist;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ src/com/example/youtubeplaylist/ApiKey.java
@@ -0,0 +1,5 @@
+package com.example.youtubeplaylist;
+
+public class ApiKey {
+	public static final String YOUTUBE_API_KEY = "AIzaSyCqJqQjQXrBRBteY6kgY3Y-zYP6thraTq8";
+}
